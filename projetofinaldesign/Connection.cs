d1a80cfe8328a4bb1666f1a2572c05490f609f03
lucas <lucas.lopesx1@gmail.com>
2021-11-09 11:16:39 -0300

    Update Connection.cs

--- projetofinaldesign/Connection.cs
+++ projetofinaldesign/Connection.cs
@@ -20,7 +20,9 @@ namespace projetofinaldesign
         public void Connect()
         {
             cn.ConnectionString = "SERVER = DESKTOP-GM7EVH8\\SQLEXPRESS; Database=Perguntas; UID=sa; PWD=1234;";
+            //oi
             cn.Open();
+
         }
 
         public void Disconnect()
