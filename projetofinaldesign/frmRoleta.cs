cb154462db322c326397631084b277d4958fdcf8
GustavoLuizGit <glogustavo4321@gmail.com>
2021-11-14 19:17:55 -0300

    Update Roleta
    Aumentando velocidade e tirando o txt que mostra seu valor

--- projetofinaldesign/frmRoleta.cs
+++ projetofinaldesign/frmRoleta.cs
@@ -31,7 +31,7 @@ namespace projetofinaldesign
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            aGauge1.Value = aGauge1.Value + 10;
+            aGauge1.Value = aGauge1.Value + 40;
             if (aGauge1.Value == 360 || aGauge1.Value > 360)
                 aGauge1.Value = 0;
         }
@@ -39,7 +39,6 @@ namespace projetofinaldesign
         {
             txtJogador1.Text = data.List().Tables[0].Rows[0].ItemArray[1].ToString();
             txtjogador2.Text = data.List().Tables[0].Rows[1].ItemArray[1].ToString();
-
 
         }
         private void CmdParar_Click(object sender, EventArgs e)
@@ -50,25 +49,22 @@ namespace projetofinaldesign
             int valorRoleta = int.Parse(aGauge1.Value.ToString());
             if (valorRoleta < 90)
             {
-                txtValor.Text = "1";
                 idPergunta = 1;
             }else if (valorRoleta < 180)
             {
-                txtValor.Text = "2";
                 idPergunta = 2;
             }
             else if (valorRoleta < 270)
             {
-                txtValor.Text = "3";
                 idPergunta = 3;
             }
             else if (valorRoleta < 360)
             {
-                txtValor.Text = "4";
                 idPergunta = 4;
             }
 
             frmPergunta fp = new frmPergunta(this.categoria, this.idJogador, this.idPergunta);
+            Hide();
             fp.ShowDialog();
         }
     }
